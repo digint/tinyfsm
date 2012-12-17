e3fb894425c6063166818897b189292be9d392fb
Axel Burri <axel@tty0.ch>
2012-12-17 17:14:33 +0100

    fixed email address

--- include/tinyfsm.hpp
+++ include/tinyfsm.hpp
@@ -1,7 +1,7 @@
 /*
  * TinyFSM - Tiny Finite State Machine Processor
  *
- * Copyright 2012 Axel Burri <axel@digint.ch>
+ * Copyright 2012 Axel Burri <axel@tty0.ch>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
