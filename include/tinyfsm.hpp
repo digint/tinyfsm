digint/tinyfsm

commit cf4a7467daf95c00c0526030f51f7947f6edac0b

refactoring reset functionality; add StateList<S...>; add resetting_switch example
Makes tinyfsm more intuitive and improves usability:

 - start does not call reset any more, but only sets initial state.
 - Fsm::reset() can be overridden by state machine class.

include/tinyfsm.hpp
@@ -85,27 +85,35 @@ namespace tinyfsm
   template<typename F>
   class Fsm
   {
-    typedef F *       state_ptr_t;
-    typedef F const * state_ptr_const_t;
+  public:
+
+    using fsmtype = Fsm<F>;
+    using state_ptr_t = F *;
 
     static state_ptr_t current_state_ptr;
 
-  protected:
-
     template<typename S>
     static constexpr S const & state(void) {
+      static_assert(std::is_same< fsmtype, typename S::fsmtype >::value,
+                    "accessing state of different state machine");
       return state_instance<S>;
     }
 
-
-  /// state machine functions (should not be used in state class)
+  /// state machine functions
   public:
 
-    static void reset(void);
+    // explicitely specialized in FSM_INITIAL_STATE macro
+    static void set_initial_state();
 
-    static void start(void) {
-      F::reset();
+    static void reset() { };
+
+    static void enter() {
       current_state_ptr->entry();
+    }
+
+    static void start() {
+      set_initial_state();
+      enter();
     }
 
     template<typename E>
@@ -113,27 +121,8 @@ namespace tinyfsm
       current_state_ptr->react(event);
     }
 
-    static state_ptr_const_t get_current_state(void) {
-      return current_state_ptr;
-    }
 
-    template<typename S>
-    static void set_current_state(void) {
-      current_state_ptr = &state_instance<S>;
-    }
-
-    static void enter(void) {
-      current_state_ptr->entry();
-    }
-
-    template<typename S>
-    static void enter(void) {
-      current_state_ptr = &state_instance<S>;
-      current_state_ptr->entry();
-    }
-
-
-  /// state transition functions (for use in state class)
+  /// state transition functions
   protected:
 
     template<typename S>
@@ -175,12 +164,10 @@ namespace tinyfsm
   template<typename... FF>
   struct FsmList;
 
-  template<>
-  struct FsmList<>
-  {
+  template<> struct FsmList<> {
+    static void set_initial_state() { }
     static void reset() { }
     static void enter() { }
-
     template<typename E>
     static void dispatch(E const &) { }
   };
@@ -188,36 +175,56 @@ namespace tinyfsm
   template<typename F, typename... FF>
   struct FsmList<F, FF...>
   {
+    using fsmtype = Fsm<F>;
+
+    static void set_initial_state() {
+      fsmtype::set_initial_state();
+      FsmList<FF...>::set_initial_state();
+    }
+
     static void reset() {
       F::reset();
       FsmList<FF...>::reset();
     }
 
     static void enter() {
-      F::enter();
+      fsmtype::enter();
       FsmList<FF...>::enter();
     }
 
     static void start() {
-      reset();
+      set_initial_state();
       enter();
     }
 
     template<typename E>
     static void dispatch(E const & event) {
-      F::template dispatch<E>(event);
+      fsmtype::template dispatch<E>(event);
       FsmList<FF...>::template dispatch<E>(event);
+    }
+  };
+
+  // --------------------------------------------------------------------------
+
+  template<typename... SS> struct StateList;
+  template<> struct StateList<> {
+    static void reset() { }
+  };
+  template<typename S, typename... SS>
+  struct StateList<S, SS...>
+  {
+    static void reset() {
+      state_instance<S> = S();
+      StateList<SS...>::reset();
     }
   };
 } /* namespace tinyfsm */
 
 
-#define FSM_INITIAL_STATE(_FSM, _STATE)          \
-namespace tinyfsm {                              \
-  template<> void Fsm< _FSM >::reset(void) {     \
-    set_current_state< _STATE >();               \
-  }                                              \
-}
+#define FSM_INITIAL_STATE(_FSM, _STATE)                               \
+  template<> void tinyfsm::Fsm< _FSM >::set_initial_state(void) {     \
+    current_state_ptr = &state_instance< _STATE >;                    \
+  }
 
 
 #endif /* TINYFSM_HPP_INCLUDED */

examples/api/.gitignore
@@ -1,4 +1,5 @@
 *.o
 *.d
 simple_switch
+resetting_switch
 multiple_switch

examples/api/resetting_switch.cpp
@@ -17,6 +17,8 @@ class Switch
 : public tinyfsm::Fsm<Switch>
 {
 public:
+  static void reset(void);   /* implemented below */
+
   /* default reaction for unhandled events */
   void react(tinyfsm::Event const &) { };
 
@@ -32,16 +34,31 @@ public:
 class On
 : public Switch
 {
-  void entry() override { std::cout << "* Switch is ON" << std::endl; };
+public:
+  On() : counter(0) { std::cout << "** RESET State=On" << std::endl; }
+  void entry() override { counter++; std::cout << "* Switch is ON, counter=" << counter << std::endl; };
   void react(Toggle const &) override { transit<Off>(); };
+private:
+  int counter;
 };
 
 class Off
 : public Switch
 {
-  void entry() override { std::cout << "* Switch is OFF" << std::endl; };
+public:
+  Off() : counter(0) { std::cout << "** RESET State=Off" << std::endl; }
+  void entry() override { counter++; std::cout << "* Switch is OFF, counter=" << counter << std::endl; };
   void react(Toggle const &) override { transit<On>(); };
+private:
+  int counter;
 };
+
+
+void Switch::reset() {
+  std::cout << "** RESET Switch" << std::endl;
+  // reset all states (calls constructor on all states in list)
+  tinyfsm::StateList<Off, On>::reset();
+}
 
 FSM_INITIAL_STATE(Switch, Off)
 
@@ -59,7 +76,6 @@ using fsm_handle = Switch;
 //
 int main()
 {
-  // instantiate events
   Toggle toggle;
 
   fsm_handle::start();
@@ -67,12 +83,16 @@ int main()
   while(1)
   {
     char c;
-    std::cout << std::endl << "t=Toggle, q=Quit ? ";
+    std::cout << std::endl << "t=Toggle, r=Restart, q=Quit ? ";
     std::cin >> c;
     switch(c) {
     case 't':
       std::cout << "> Toggling switch..." << std::endl;
       fsm_handle::dispatch(toggle);
+      break;
+    case 'r':
+      fsm_handle::reset();
+      fsm_handle::start();
       break;
     case 'q':
       return 0;

commit 9d0e1c3f83596acf43bd69b53fa69ec172891ced

fix regression: FSM_INITIAL_STATE: define template specialization in "namespace tinyfsm" block
Due to a bug, gcc < 7.0 (tested with 5.4.0, 6.4.0) fails with:

    error: specialization of ‘template<class F> static void tinyfsm::Fsm<F>::set_initial_state()’ in different namespace [-fpermissive]

Fixed by defining template specialization in "namespace tinyfsm"
block, which should work for all versions of gcc.

Tested with: gcc-6.4.0, gcc-7.3.0, clang-6.0.0

See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56480

Regression from commit: cf4a7467daf95c00c0526030f51f7947f6edac0b

include/tinyfsm.hpp
@@ -232,9 +232,10 @@ namespace tinyfsm
 
 
 #define FSM_INITIAL_STATE(_FSM, _STATE)                               \
-  template<> void tinyfsm::Fsm< _FSM >::set_initial_state(void) {     \
+namespace tinyfsm {                                                   \
+  template<> void Fsm< _FSM >::set_initial_state(void) {              \
     current_state_ptr = &_state_instance< _STATE >::value;            \
-  }
-
+  }                                                                   \
+}
 
 #endif /* TINYFSM_HPP_INCLUDED */